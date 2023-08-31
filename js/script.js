8ef0aebc47ecf880f16dea580697c7c92f5b1dd2
Lorenzo Cutrera <lorenzoq3ra@gmail.com>
2023-08-31 17:31:17 +0200

    add  prompt

--- js/script.js
+++ js/script.js
@@ -4,13 +4,23 @@
 */
 
 
-//stabilire i numeri pari e quelli dispari
+let user_uno;
+let user_pc; 
+
+
 
 // domandare all'utente di scegliere un numero pari o dispari
+user_uno = prompt('Scegli tra pari o dispari');
 //domandare all'utente un numero da 1 a 5 
+if(user_uno = 'pari'){
+    Number.prompt('Adesso scegli un numero pari da 1 a 5');
+}else{
+    Number.prompt('Adesso scegli un numero dispari da 1 a 5');
+}
+
 
 //il pc genera un numero random da 1 a 5
-
+user_pc = ((Math.random() * 5) + 1);
 //sommiamo i numeri dati/generati
 
 //stabiliamo se il risultato è pari o dispari
