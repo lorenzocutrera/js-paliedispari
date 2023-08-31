175d159104ecbe0e8f3e7767e2bb7acdd633d065
Lorenzo Cutrera <lorenzoq3ra@gmail.com>
2023-08-31 18:07:26 +0200

    add floor

--- js/script.js
+++ js/script.js
@@ -6,23 +6,23 @@
 
 let user_uno;
 let user_pc; 
-
-
+let user_uno_number;
+let somma;
 
 // domandare all'utente di scegliere un numero pari o dispari
 user_uno = prompt('Scegli tra pari o dispari');
+console.log(user_uno);
 //domandare all'utente un numero da 1 a 5 
-if(user_uno = 'pari'){
-    Number.prompt('Adesso scegli un numero pari da 1 a 5');
-}else{
-    Number.prompt('Adesso scegli un numero dispari da 1 a 5');
-}
-
+user_uno_number = prompt('Adesso scegli un numero da 1 a 5')
+console.log(user_uno_number);
 
 //il pc genera un numero random da 1 a 5
-user_pc = ((Math.random() * 5) + 1);
-//sommiamo i numeri dati/generati
+user_pc = Math.floor((Math.random() * 5) + 1);
+console.log(user_pc);
 
+//sommiamo i numeri dati/generati
+somma = Number(user_uno_number += user_pc)
+console.log(somma);
 //stabiliamo se il risultato è pari o dispari
 
 //se la scelta iniziale (pari/dispari) dell'utente è uguale al risultato l'utente vince
